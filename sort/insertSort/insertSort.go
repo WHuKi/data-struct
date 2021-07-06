5406c61d86e6270025089f4cc232fed649efa87a
huxiaoke <huxiaoke515@163.com>
2021-07-06 14:25:56 +0800

    feat(sort): 插入排序
    插入排序修改

--- sort/insertSort/insertSort.go
+++ sort/insertSort/insertSort.go
@@ -1,18 +1,18 @@
 package insertSort
 
-// insert sort
+/*
+InsertSort insert sort
+适用于部分有序，规模比较小的数组
+*/
 func InsertSort(arr []int) []int {
 	if len(arr) <= 1 {
 		return arr
 	}
 
 	for i := 1; i < len(arr); i++ {
-		x := arr[i]
-		j := i - 1
-		for ; j >= 0 && x < arr[j]; j-- {
-			arr[j+1] = arr[j]
+		for j := i; j > 0 && arr[j-1] > arr[j]; j-- {
+			arr[j-1], arr[j] = arr[j], arr[j-1]
 		}
-		arr[j+1] = x
 	}
 
 	return arr
--- sort/selectSort/selectSort.go
+++ sort/selectSort/selectSort.go
@@ -1,16 +1,19 @@
 package selectSort
 
 func SelectSort(arr []int) []int {
-	for i := 0; i < len(arr)-1; i++ {
-		k := i
+	if len(arr) < 1 {
+		return arr
+	}
+
+	for i := 0; i < len(arr); i++ {
+		min := arr[i]
 		for j := i + 1; j < len(arr); j++ {
-			if arr[k] > arr[j] {
-				k = j
+			if arr[j] < min {
+				min = arr[j]
+				arr[i], arr[j] = arr[j], arr[i]
 			}
 		}
-		if k != i {
-			arr[i], arr[k] = arr[k], arr[i]
-		}
 	}
+
 	return arr
 }
